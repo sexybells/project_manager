a31006e3538d475e2cb6e7f0ecb601590f5dd73e
sexybell <nguyenhaid98@yahoo,cin>
2023-09-12 01:16:02 +0700

    update

--- src/components/users/userList.js
+++ src/components/users/userList.js
@@ -1,6 +1,40 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import firebase from '../../helper/firebaseConfig';
+import { Container } from 'react-bootstrap';
 const UserList = () => {
+
+    const users = firebase.firestore().collection('Users');
+    const [userList, setUserList] = useState([])
+    const fetchData = async () => {
+        await users.get().then((querySnapshot) => {
+            if (querySnapshot.size > 0) {
+                const result = [];
+                querySnapshot.docs.map((v) => {
+                    const data = v.data();
+                    const params = {
+                        name: data.name,
+                        email: data.email,
+                        id: v.id,
+                        role: data.role,
+                        department: data.department
+                    }
+                    result.push(params);
+                })
+                setUserList(result);
+            }
+        })
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, [])
+
+    return (
+        <Container>
+
+        </Container>
+    )
+
 
 }
 
